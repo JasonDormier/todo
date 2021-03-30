b6da9c3d3ae8dfc7cfc2eeae5d987f7e4a57ce3d
JasonDormier <jmdormier@gmail.com>
2021-03-29 20:44:21 -0700

    lab complete

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ToDo from './components/todo/todo.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <>
+      <ToDo />
+    </>
+  );
+}
