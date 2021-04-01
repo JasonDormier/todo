f616695e89df08cf55a4badca38ca9f189b09b03
JasonDormier <jmdormier@gmail.com>
2021-03-31 18:20:39 -0700

    mostly complete

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import ToDo from './components/todo/todo.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import SettingsContext from './context/settings/context.js';
 
 
 
 export default function App() {
   return (
+    <SettingsContext>
     <>
       <ToDo />
     </>
+    </SettingsContext>
   );
 }
---
+++ src/hooks/ajax.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const useAjax = () => {
+    const [options, request] = useState({});
+    const [response, setResponse] = useState({});
+
+
+
+    useEffect(() => {
+        async function ajax() {
+            if (!options.url) return;
+            let res = await axios(options);
+            setResponse(res.data);
+        }
+        ajax();
+    }, [options])
+
+    return [request, response];
+}
+
+export default useAjax;
