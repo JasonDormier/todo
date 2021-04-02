0bb5d366aa875681bb782eb0bb94e64441ca80fd
JasonDormier <jmdormier@gmail.com>
2021-04-02 08:36:36 -0700

    update

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,66 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import jwt from 'jsonwebtoken';
-import useAjax from '../hooks/useAjax.js';
-import cookie from 'react-cookies';
+import { useContext } from 'react';
+import { AuthContext } from "../../context/AuthProvider.js";
+import jwt from "jsonwebtoken";
 
-export const AuthContext = React.createContext();
-
-const API_URL = 'https://api-js401.herokuapp.com';
-
-function AuthProvider(props) {
-
-  let [user, setUser] = useState({});
-  let [token, setToken] = useState('');
-  let [request, response] = useAjax();
-
-  useEffect(() => {
-    console.log(response);
-    if (_isValidUser(response.token)) {
-      setUser(response.user);
-      setToken(response.token);
-      cookie.save('auth', response.token);
-    }
-  }, [response]);
-
-  useEffect(() => {
-    let token = cookie.load('auth');
-    if (token) {
-      setToken(token);
-    }
-  }, []);
-
-  const _isValidUser = (token) => {
-    const validUser = jwt.decode(token);
-    if (validUser) {
-      if (validUser.username === user.username) return true;
-    } else {
-      return false
-    }
-  }
-
-  const login = (username, password) => {
-    // basic authentication header options
-    let options = {
-      url: `${API_URL}/signin`,
-      method: 'POST',
-      headers: {
-        Authorization: `Basic ${btoa(`${username}:${password}`)}`
-      }
-    }
-    request(options);
-  }
-
-  const logout = () => {
-    setUser({});
-    setToken('');
-    cookie.remove('auth');
-  }
-
-  return (
-    <AuthContext.Provider value={{ user, token, login, logout }}>
-      {props.children}
-    </AuthContext.Provider>
-  )
+function If({ condition, children }) {
+    return condition ? children : null;
 }
 
-export default AuthProvider;
+function Auth({ capability, children }){
+    let context = useContext(AuthContext);
+    const isAuthorized = (capability) => {
+        let userData = jwt.decode(context.token);
+        if (userData) {
+            return userData.capabilities.includes(capability) ? true : false;
+        }
+        return false
+    }
+
+    return(
+        <>
+        <If condition={isAuthorized(capability)}>
+          {children}
+        </If>
+        <If condition={!isAuthorized(capability)}>
+          <h3>Not Authorized</h3>
+        </If>
+      </>  
+    )
+}
+
+export default Auth;
